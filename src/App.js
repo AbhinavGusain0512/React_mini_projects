29759edc5c0c21d6262b23d3d4bc72c128595851
ABHINAVGUSAIN0512 <abhinavgusain0512@gmail.com>
2024-03-21 17:52:36 +0530

    added load-more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random_color';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
+import LoadMore from "./components/load-more";
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
       {/* <Accordian/> */}
       {/* <RandomColor/> */}
       {/* <StarRating /> */}
-      <ImageSlider url={"https://picsum.photos/v2/list"}
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"}
         page={"1"}
-        limit={"10"}/>
+        limit={"10"}/> */}
+
+      <LoadMore />
     </div>
   );
 }
